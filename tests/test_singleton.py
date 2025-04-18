287b6cbfb29c5f8b070e91ef76d489207c94653c
glory-kasongo-03 <glorykasongo03@gmail.com>
2025-04-18 22:45:40 +0200

    Delete tests directory

--- tests/test_singleton.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from creational_patterns.singleton import DatabaseConnection
-# The Singleton pattern ensures that a class has only one instance and provides a global point of access to it.
-
-class TestSingletonPattern(unittest.TestCase):
-    def test_singleton_instance(self):
-        db1 = DatabaseConnection()
-        db2 = DatabaseConnection()
-        self.assertIs(db1, db2)  # Both should refer to the same instance
