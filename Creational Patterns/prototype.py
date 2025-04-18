e73db9f0da4fe1ef718b47463407b875b94c44bd
glory-kasongo-03 <glorykasongo03@gmail.com>
2025-04-18 22:46:10 +0200

    Delete Creational Patterns directory

--- Creational Patterns/prototype.py
+++
@@ -1,9 +0,0 @@
-import copy
-
-class Report:
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
-
-    def clone(self):
-        return copy.deepcopy(self)
--- Creational Patterns/singleton.py
+++
@@ -1,8 +0,0 @@
-class DatabaseConnection:
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(DatabaseConnection, cls).__new__(cls)
-            print("Database connection established.")
-        return cls._instance
