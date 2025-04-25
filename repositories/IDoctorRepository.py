13b798469d8a1f896b66dbc95e70d52977e8483c
glory-kasongo-03 <glorykasongo03@gmail.com>
2025-04-26 00:45:22 +0200

    deleted

--- repositories/IDoctorRepository.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from Repository_Interface_Design import IRepository
-from doctor import Doctor
-
-class IDoctorRepository(IRepository[Doctor, int]):
-    def find_by_specialization(self, specialization: str) -> List[Doctor]:
-        pass
--- repositories/IPatientRepository.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from Repository_Interface_Design import IRepository
-from patient import Patient
-
-class IPatientRepository(IRepository[Patient, int]):
-    def find_by_email(self, email: str) -> Patient:
-        pass
