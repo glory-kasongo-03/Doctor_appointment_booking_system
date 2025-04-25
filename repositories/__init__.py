551aca842418e4f88bcc81b6d873a53503e12950
glory-kasongo-03 <glorykasongo03@gmail.com>
2025-04-26 01:34:53 +0200

    updated

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -10,12 +10,21 @@ __all__ = [
 ]
 
 # Import the specific components to make them accessible through the package
+
+# Import the repository interfaces
+from .repository_interface_design import IRepository
+from repositories import i_admin_repository
+from repositories import i_patient_repository
+from repositories import i_doctor_repository
+from repositories import i_appointment_repository
+from repositories import i_notification_repository
+
+# Import the repository interfaces
 from .i_patient_repository import IPatientRepository
 from .i_doctor_repository import IDoctorRepository
 from .i_appointment_repository import IAppointmentRepository
 from .i_notification_repository import INotificationRepository
 from .i_admin_repository import IAdminRepository
-from .repository_interface_design import IRepository
 
 # Import the in-memory implementations
 from .in_memory import patient_in_memory
