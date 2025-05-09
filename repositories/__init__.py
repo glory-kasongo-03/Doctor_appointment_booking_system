6ba8aa4258bb8aa441b55309b394fbe72b373ca7
glory-kasongo-03 <glorykasongo03@gmail.com>
2025-05-09 20:39:51 +0200

    updated ini file

--- repositories/__init__.py
+++ repositories/__init__.py
@@ -1,31 +1,38 @@
 # repositories/__init__.py
 
-# Explicitly define what should be exported when using `from repositories import *`
 __all__ = [
-    "i_patient_repository", "i_doctor_repository", "i_appointment_repository",
-    "i_notification_repository", "i_admin_repository",
-    "patient_in_memory", "doctor_in_memory",
-    "appointment_in_memory", "notification_in_memory",
-    "admin_in_memory"
+    # Interfaces
+    "IPatientRepository", "IDoctorRepository", "IAppointmentRepository",
+    "IAdminRepository", "INotificationRepository",
+
+    # In-memory Implementations
+    "InMemoryPatientRepository", "InMemoryDoctorRepository",
+    "InMemoryAppointmentRepository", "InMemoryAdminRepository",
+    "InMemoryNotificationRepository",
+
+    # Services
+    "PatientService", "DoctorService", "AppointmentService",
+    "AdminService", "NotificationService"
 ]
 
-# Import the specific components to make them accessible through the package
+# Interfaces
+from i_patient_repository import IPatientRepository
+from i_doctor_repository import IDoctorRepository
+from i_appointment_repository import IAppointmentRepository
+from i_admin_repository import IAdminRepository
+from i_notification_repository import INotificationRepository
 
-# Import the repository interfaces
-from .repository_interface_design import IRepository
-from repositories import i_admin_repository
-from repositories import i_patient_repository
-from repositories import i_doctor_repository
-from repositories import i_appointment_repository
-from repositories import i_notification_repository
+# In-memory Repositories
+from .in_memory.patient_in_memory import InMemoryPatientRepository
+from .in_memory.doctor_in_memory import InMemoryDoctorRepository
+from .in_memory.appointment_in_memory import InMemoryAppointmentRepository
+from .in_memory.admin_in_memory import InMemoryAdminRepository
+from .in_memory.notification_in_memory import InMemoryNotificationRepository
 
-# Import the repository interfaces
-from .i_patient_repository import IPatientRepository
-from .i_doctor_repository import IDoctorRepository
-from .i_appointment_repository import IAppointmentRepository
+# Services (assuming they are located in a sibling services/ folder)
+from services.patient_service import PatientService
+from services.doctor_service import DoctorService
+from services.appointment_service import AppointmentService
+from services.admin_service import AdminService
+from services.notification_service import NotificationService
 
-
-# Import the in-memory implementations
-from .in_memory import patient_in_memory
-from .in_memory import doctor_in_memory
-from .in_memory import appointment_in_memory
